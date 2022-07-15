83d21619f227fdcee76f76fcfa43cb6a0c86d1d3
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2022-07-15 22:25:53 +0530

    added routes

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const db = require("./db");
 const pizza = require("./models/PizzaModel");
-
+const pizzasRoute = require("./routes/pizzasRoute");
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("Server is conncetd !!!");
-});
-
-app.get("/getallpizzas", (req, res) => {
-	pizza.find({}, (err, docs) => {
-		if (err) {
-			return res.status(500).send(err);
-		} else {
-			// console.log(docs);
-			res.status(200).send(docs);
-		}
-	});
-});
+app.use("/api/pizzas", pizzasRoute);
 
 const port = process.env.PORT || 5000;
 
