bfff70710f58e3a1501f88b918302a790532a939
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2022-08-01 20:01:50 +0530

    added the route and logic for the delivery of orders

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -86,4 +86,17 @@ router.get("/getallorders", async (req, res) => {
 	}
 });
 
+//delivering orders
+
+router.post("/deliverorder", async (req, res) => {
+	const orderid = req.body.orderid;
+	try {
+		const order = await Order.findOne({ _id: orderid });
+		order.isDelivered = true;
+		await order.save();
+		res.send("Order Delivered Successfully");
+	} catch (error) {
+		return res.status(400).json({ message: error });
+	}
+});
 module.exports = router;
